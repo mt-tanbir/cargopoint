ef9ce8f26793ba419e9a90e03d774d35cc051224
mt-tanbir <97947584+mt-tanbir@users.noreply.github.com>
2023-10-12 12:08:14 +0600

    jquery added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,46 @@
-function openSearch() {
-  document.getElementById("myOverlay").style.display = "block";
-}
+$(document).ready(function () {
+  $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
 
-function closeSearch() {
-  document.getElementById("myOverlay").style.display = "none";
-}
+$(document).ready(function () {
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000,
+    offset: 70,
+    beginAt: 100,
+    formatter: function (n) {
+      return n.replace(/,/g, '.');
+    }
+  });
+});
