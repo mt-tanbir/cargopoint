d42c9cba7a8650e0f99a04bbfdc02b672de22bb1
mt-tanbir <97947584+mt-tanbir@users.noreply.github.com>
2023-10-14 14:17:38 +0600

    about page added

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,15 +32,3 @@ $(document).ready(function () {
     ]
   });
 });
-
-$(document).ready(function () {
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1000,
-    offset: 70,
-    beginAt: 100,
-    formatter: function (n) {
-      return n.replace(/,/g, '.');
-    }
-  });
-});
