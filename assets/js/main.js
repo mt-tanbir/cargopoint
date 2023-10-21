3f999ccdd8343ad07db1af2e2da6b799fdb8f429
mt-tanbir <97947584+mt-tanbir@users.noreply.github.com>
2023-10-21 17:11:33 +0600

    section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,4 +38,40 @@ $(document).ready(function () {
     time: 1400
   });
 
+  $('.service-area-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
+  });
+
 });
